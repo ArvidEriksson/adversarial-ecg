256a9ba751939222c47554ddaec1fadb0283d85a
Arvid Eriksson <arvid.f.eriksson@gmail.com>
2025-02-17 09:48:39 +0100

    updated refactoring

--- autopgd/autopgd_base.py
+++ autopgd/autopgd_base.py
@@ -12,8 +12,8 @@ import torch.nn.functional as F
 import math
 import random
 
-from other_utils import L0_norm, L1_norm, L2_norm
-from checks import check_zero_gradients
+from autopgd.other_utils import L0_norm, L1_norm, L2_norm
+from autopgd.checks import check_zero_gradients
 
 
 def L1_projection(x2, y2, eps1):
